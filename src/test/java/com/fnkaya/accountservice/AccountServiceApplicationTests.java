a276838c70245d4429d1459ca437935f78d1d123
Furkan Kaya <fnkaya94@gmail.com>
2021-07-07 19:16:30 +0300

    Test

--- src/test/java/com/fnkaya/accountservice/AccountServiceApplicationTests.java
+++ src/test/java/com/fnkaya/accountservice/AccountServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package com.fnkaya.accountservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AccountServiceApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        String jsonResponse = "{\"id\": 1,\"email\": \"test1@gmail.com\",\"password\": \"123456\"}";
+
+        mockMvc.perform(get("/api/v1/account/1"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().json(jsonResponse));
     }
 
 }
