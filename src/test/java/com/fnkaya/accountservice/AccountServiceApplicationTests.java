e42732df82961c49b9ad797cd3e982ba903c0742
Furkan Kaya <fnkaya94@gmail.com>
2021-07-07 22:15:07 +0300

    build.yml

--- src/test/java/com/fnkaya/accountservice/AccountServiceApplicationTests.java
+++ src/test/java/com/fnkaya/accountservice/AccountServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.fnkaya.accountservice;
 
+import com.fasterxml.jackson.core.JsonFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
