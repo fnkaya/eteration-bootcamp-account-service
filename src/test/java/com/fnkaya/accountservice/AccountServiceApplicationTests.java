6a40fa3820c1f9ac44cfff83b3f98f65b4130395
Furkan Kaya <fnkaya94@gmail.com>
2021-07-08 10:53:29 +0300

    Object mapper

--- src/test/java/com/fnkaya/accountservice/AccountServiceApplicationTests.java
+++ src/test/java/com/fnkaya/accountservice/AccountServiceApplicationTests.java
@@ -1,6 +1,8 @@
 package com.fnkaya.accountservice;
 
 import com.fasterxml.jackson.core.JsonFactoryBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fnkaya.accountservice.model.Account;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,11 +23,11 @@ class AccountServiceApplicationTests {
 
     @Test
     void contextLoads() throws Exception {
-        String jsonResponse = "{\"id\": 1,\"email\": \"test1@gmail.com\",\"password\": \"123456\"}";
-
+        Account account = Account.builder().id(1L).email("test1@gmail.com").password("123456").build();
+        ObjectMapper objMapper = new ObjectMapper();
         mockMvc.perform(get("/api/v1/account/1"))
                 .andDo(print()).andExpect(status().isOk())
-                .andExpect(content().json(jsonResponse));
+                .andExpect(content().json(objMapper.writeValueAsString(account)));
     }
 
 }
